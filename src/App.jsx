b5f7dab09417aa92fc6ecb89129a3c6920745539
Subodh-Bny <adhikarisubodh77@gmail.com>
2024-09-16 19:48:41 +0545

    portfolio using three.js

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import About from "./section/About";
+import Clients from "./section/Clients";
+import Contact from "./section/Contact";
+import Experinece from "./section/Experience";
+import Footer from "./section/Footer";
+import Hero from "./section/Hero";
+import Navbar from "./section/Navbar";
+import Projects from "./section/Projects";
+
+const App = () => {
+  return (
+    <main className="max-w-7xl mx-auto relative">
+      <Navbar />
+      <Hero />
+      <About />
+      <Projects />
+      <Clients />
+      <Experinece />
+      <Contact />
+      <Footer />
+    </main>
+  );
+};
+
+export default App;
---
+++ src/constants/index.js
@@ -0,0 +1,115 @@
+export const navLinks = [
+  {
+    id: 1,
+    name: "Home",
+    href: "#home",
+  },
+  {
+    id: 2,
+    name: "About",
+    href: "#about",
+  },
+  {
+    id: 3,
+    name: "Work",
+    href: "#work",
+  },
+  {
+    id: 4,
+    name: "Contact",
+    href: "#contact",
+  },
+];
+
+export const clientReviews = [
+  // {
+  //   id: 1,
+  //   name: "Emily Johnson",
+  //   position: "Marketing Director at GreenLeaf",
+  //   img: "assets/review1.png",
+  //   review:
+  //     "Working with Adrian was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.",
+  // },
+];
+
+export const myProjects = [
+  {
+    title: "project",
+    desc: "",
+    subdesc:
+      "Built as a unique Software-as-a-Service app with Next.js 14, Tailwind CSS, TypeScript, Framer Motion and Convex, Podcastr is designed for optimal performance and scalability.",
+    href: "#",
+    texture: "/textures/project/project1.mp4",
+    logo: "/assets/project-logo1.png",
+    logoStyle: {
+      backgroundColor: "#2A1816",
+      border: "0.2px solid #36201D",
+      boxShadow: "0px 0px 60px 0px #AA3C304D",
+    },
+    spotlight: "/assets/spotlight1.png",
+    tags: [
+      {
+        id: 1,
+        name: "React.js",
+        path: "/assets/react.svg",
+      },
+      {
+        id: 2,
+        name: "TailwindCSS",
+        path: "assets/tailwindcss.png",
+      },
+      {
+        id: 3,
+        name: "TypeScript",
+        path: "/assets/typescript.png",
+      },
+    ],
+  },
+];
+
+export const calculateSizes = (isSmall, isMobile, isTablet) => {
+  return {
+    deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
+    deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
+    cubePosition: isSmall
+      ? [4, -5, 0]
+      : isMobile
+      ? [5, -5, 0]
+      : isTablet
+      ? [5, -5, 0]
+      : [9, -5.5, 0],
+    reactLogoPosition: isSmall
+      ? [3, 4, 0]
+      : isMobile
+      ? [5, 4, 0]
+      : isTablet
+      ? [5, 4, 0]
+      : [12, 3, 0],
+    ringPosition: isSmall
+      ? [-5, 7, 0]
+      : isMobile
+      ? [-10, 10, 0]
+      : isTablet
+      ? [-12, 10, 0]
+      : [-24, 10, 0],
+    targetPosition: isSmall
+      ? [-5, -10, -10]
+      : isMobile
+      ? [-9, -10, -10]
+      : isTablet
+      ? [-11, -7, -10]
+      : [-13, -13, -10],
+  };
+};
+
+export const workExperiences = [
+  {
+    id: 1,
+    name: "Void Nepal Pvt. Ltd.",
+    pos: "Frontend Developer Intern",
+    duration: "May 2024 - Sep 2024",
+    title: "",
+    icon: "/assets/voidNepal.jpg",
+    animation: "victory",
+  },
+];
